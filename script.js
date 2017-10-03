923647243e5809c7bfd246944699ffe7e6c8a3b6
Magda <magda@motta.com.pl>
2017-10-03 12:48:29 +0200

    dodanie i usuniecie cookies

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
+/* global expirationDateCookie, expirationDataCookie */
+
 /*alert('uwagggggaaa!!!!');*/
+//postaæ cookies: klucz=wartosc;klucz2=wartosc2;
+//"path=/" ciastko które jest dostêpne w ka¿dym subfolderze (bez tego tylko w tym folderze w którym zosta³ stworzony)
+//expires= data wa¿noœci ciasteczka po zakoñczeniu sesji. Przy tej opcji musimy tworzyæ var x = new Date(); np. document.cookie = "imie=Magda;max-age="+expirationDataCookie.toUTCString();
+//max-age=masymalny czas wa¿nosci cookie (UWGA w sekundach)
 
 window.onload = function()
 {
 	var box = document.getElementById("box");
 	var zrobCiasteczko = document.getElementById("zrobCiasteczko");
+        var usunCiasteczko = document.getElementById("usunCiasteczko");
+        
+	box.innerHTML = document.cookie;
 
-	
 
 	zrobCiasteczko.onclick = function()
 	{
-		document.cookie = "imie=Magda;";
-		box.innerHTML = document.cookie;
-		//var expirationDateCookie = new Date();
-		//expirationDateCookie.setDate(expirationDateCookie.getDate() + 30);
-		//document.cookie = "nazwa=ciacho;expires="+expirationDateCookie.toUTCString();
+            var expirationDataCookie = new Date();
+            expirationDataCookie.setDate(expirationDateCookie.getDate()+30);
+                        
+            document.cookie = "imie=Magda;max-age="+60*60+";path=/";
+            document.cookie = "nazwisko=Zdybel;max-age="+60*60+";path=/";
+
 	};
+        usunCiasteczko.onclick = function()
+        {
+            var expirationDataCookie = new Date();
+            expirationDataCookie.setDate(expirationDateCookie.getDate()-1);
+            
+            document.cookie = "imie=;expires="+expirationDateCookie.toUTCString()+";path=/";
+        };
 };
