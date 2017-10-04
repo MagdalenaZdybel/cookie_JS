b708ca857d1c2a8368658ab29a69e73c71b33f2a
Magda <magda@motta.com.pl>
2017-10-04 13:07:08 +0200

    The end

--- script_function.js
+++ script_function.js
@@ -1,16 +1,40 @@
 /* global expirationDateCookie, nazwisko */
 
-var expirationDataCookie = new Data();
+//var expirationDataCookie = new Data();
+
+function createCookie(name, value, days)
+{
+    var expires = '';
+    
+    if (days)
+    {
+    var expirationDateCookie = new Date();
+    expirationDateCookie.setDate(expirationDateCookie.getDate()+days);
+    expires = ";expires=" + expirationDateCookie.toUTCString();
+    };
+    
+    document.cookie = name + "-" + value + expires +";path=/";
+};
 
 function removeCookie(name)
 {
-    //var expirationDateCookie = new Date();
+    var expirationDateCookie = new Date();
     expirationDateCookie.setTime(expirationDateCookie.getTime()-1);
     document.cookie = name+"=;expires="+expirationDateCookie.toUTCString()+";path=/";
 };
 
+function getCookieByName(name)
+{
+    var cookies = document.cookie.split("; ");
+    
+    for (var i = 0; i < cookies.length; i++)
+    {
+        
+    };
+};
 window.onload = function()
 {
+
 	var box = document.getElementById("box");
 	var zrobCiasteczko = document.getElementById("zrobCiasteczko");
         var usunCiasteczko = document.getElementById("usunCiasteczko");
@@ -20,11 +44,13 @@ window.onload = function()
 
 	zrobCiasteczko.onclick = function()
 	{
-           // var expirationDateCookie = new Date();
-            expirationDateCookie.setDate(expirationDateCookie.getDate()+30);
-                        
-            document.cookie = "imie=Magda;max-age="+60*60+";path=/";
-            document.cookie = "nazwisko=Zdybel;max-age="+60*60+";path=/";
+
+            
+            createCookie("imie", "Magda", 30);
+            createCookie("nazwisko", "Zdybel");
+            
+            //document.cookie = "imie=Magda;max-age="+60*60+";path=/";
+            //document.cookie = "nazwisko=Zdybel;max-age="+60*60+";path=/";
 
 	};
         usunCiasteczko.onclick = function()
